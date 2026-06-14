9ff71991af7015bb55d8419ee969bbf7ca1e08d1
cholode <2681194748@qq.com>
2026-06-14 21:50:29 +0800

    cpu指令重排的bug

--- 05.once/main.go
+++ 05.once/main.go
@@ -19,6 +19,31 @@ func Exp1() {
 	oc.Do(Print2)
 }
 
+var config string
+var ready bool
+
+func setup() {
+	config = "初始化完成的配置内容" // 写1：初始化数据
+	ready = true          // 写2：标记完成
+}
+
+func Exp2() bool { //CPU指令重排可能导致config没有被执行，高并发有概率发生，单线程不可能发生
+	go setup()
+
+	// 主 goroutine 循环等待标记
+	for !ready {
+		// 空转
+	}
+	//println(config) // 可能打印空字符串！
+	if config != "初始化完成的配置内容" {
+		fmt.Println("初始化失败")
+		return false
+	}
+	return true
+}
+
 func main() {
-	Exp1()
+	for Exp2() {
+	}
+
 }
